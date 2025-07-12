0bd711151a5c41dfbfd1c6d302601e5f73bfd0bc
Julio Cesar <juliocesar.pg92@gmail.com>
2025-07-12 22:55:08 +0200

    feat(back): :sparkles: enhance filter endpoint to utilize structured filter type and improve data fetching logic

--- back/src/index.ts
+++ back/src/index.ts
@@ -3,18 +3,18 @@ import { Hono } from "hono"
 import { cors } from "hono/cors"
 import { logger } from "hono/logger"
 import { secureHeaders } from "hono/secure-headers"
-import { validateFilterSchema } from "./models/filters.js"
+import { validateFilterSchema, type Filter } from "./models/filters.js"
 import { validator } from "hono/validator"
 import * as z from "zod/v4"
+import { fetchData, setupDb } from "./db/index.js"
 
 const app = new Hono()
 app.use(logger())
 app.use(cors())
 app.use(secureHeaders())
 
-// initialize db connection
-
-// import csv data
+// initialize db connection and import CSV data
+await setupDb()
 
 app.get(
   "/filter",
@@ -26,12 +26,16 @@ app.get(
     return result.data
   }),
   (c) => {
-    const { parameters, startDate, endDate } = c.req.valid("query")
+    const filter: Filter = c.req.valid("query")
+
+    const result = fetchData(filter)
+    if (!result) {
+      return c.json({ message: "No data found for the given filter." }, 404)
+    }
+
     return c.json({
-      parameters,
-      startDate,
-      endDate,
-      message: "Hello, Hono!",
+      message: "Data fetched successfully.",
+      data: result,
     })
   }
 )
@@ -43,7 +47,7 @@ app.get("/health", (c) => {
 serve(
   {
     fetch: app.fetch,
-    port: 3000,
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
   },
   (info) => {
     console.log(`Server is running on http://localhost:${info.port}`)
