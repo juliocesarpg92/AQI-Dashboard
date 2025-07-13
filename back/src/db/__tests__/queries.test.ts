a7bcb1cfffc48567b55cc5c14c0063fe1fe5c76e
Julio Cesar <juliocesar.pg92@gmail.com>
2025-07-13 08:45:13 +0200

    test(back): :white_check_mark: update generated queries test
    the idea is to have a base query to check against and the tests should not fail if any extra clause is added

--- back/src/db/__tests__/queries.test.ts
+++ back/src/db/__tests__/queries.test.ts
@@ -4,28 +4,34 @@ import { prepareQueryFetchData } from "../queries.js"
 
 describe("fetchDataByFilter", () => {
   test("should generate correct query when no parameters are provided", async () => {
-    const expectedQuery = "SELECT 1, * FROM air_quality_data WHERE 1=1"
+    const expectedQueryBase = "SELECT * FROM air_quality_data WHERE 1=1"
     const result = await prepareQueryFetchData({})
 
     // Verify the generated query
-    assert.strictEqual(result.query, expectedQuery)
+    assert.ok(
+      result.query.includes(expectedQueryBase),
+      "Query should include expected base query"
+    )
     assert.deepStrictEqual(result.values, [])
   })
 
   test("should generate correct query with specific parameters", async () => {
-    const expectedQuery =
-      "SELECT 1, $1 AS co_gt, $2 AS no2_gt FROM air_quality_data WHERE 1=1"
-    const values = ["co_gt", "no2_gt"]
-    const result = await prepareQueryFetchData({ parameters: values })
+    const expectedQueryBase =
+      "SELECT date, time, co_gt, no2_gt FROM air_quality_data WHERE 1=1"
+    const result = await prepareQueryFetchData({
+      parameters: ["co_gt", "no2_gt"],
+    })
 
     // Verify the generated query
-    assert.strictEqual(result.query, expectedQuery)
-    assert.deepStrictEqual(result.values, values)
+    assert.ok(
+      result.query.includes(expectedQueryBase),
+      "Query should include expected base query"
+    )
   })
 
   test("should generate correct query with date range filter", async () => {
-    const expectedQuery =
-      "SELECT 1, * FROM air_quality_data WHERE 1=1 AND date >= $1 AND date <= $2"
+    const expectedQueryBase =
+      "SELECT * FROM air_quality_data WHERE 1=1 AND date >= $1 AND date <= $2"
     const values = ["2023-01-01", "2023-12-31"]
     const result = await prepareQueryFetchData({
       startDate: values[0],
@@ -33,22 +39,29 @@ describe("fetchDataByFilter", () => {
     })
 
     // Verify the generated query
-    assert.strictEqual(result.query, expectedQuery)
+    assert.ok(
+      result.query.includes(expectedQueryBase),
+      "Query should include expected base query"
+    )
     assert.deepStrictEqual(result.values, values)
   })
 
   test("should generate correct query with parameters and date range", async () => {
-    const expectedQuery =
-      "SELECT 1, $1 AS co_gt, $2 AS no2_gt FROM air_quality_data WHERE 1=1 AND date >= $3 AND date <= $4"
-    const values = ["co_gt", "no2_gt", "2023-01-01", "2023-12-31"]
+    const expectedQueryBase =
+      "SELECT date, time, co_gt, no2_gt FROM air_quality_data WHERE 1=1 AND date >= $1 AND date <= $2"
+    const values = ["2023-01-01", "2023-12-31"]
     const result = await prepareQueryFetchData({
-      parameters: values.slice(0, 2),
-      startDate: values[2],
-      endDate: values[3],
+      parameters: ["co_gt", "no2_gt"],
+      startDate: values[0],
+      endDate: values[1],
     })
+    console.log("Generated query:", result.query)
 
     // Verify the generated query
-    assert.strictEqual(result.query, expectedQuery)
+    assert.ok(
+      result.query.includes(expectedQueryBase),
+      "Query should include expected base query"
+    )
     assert.deepStrictEqual(result.values, values)
   })
 })
