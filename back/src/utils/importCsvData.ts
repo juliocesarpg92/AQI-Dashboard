364991c28c55e2016315e9a151a7e4c6c2adcddf
Julio Cesar <juliocesar.pg92@gmail.com>
2025-07-14 22:50:56 +0200

    chore(back): :art: reorganize CSV import function

--- back/src/utils/importCsvData.ts
+++ back/src/utils/importCsvData.ts
@@ -17,95 +17,111 @@ export default function importCsvData(
     let chunks: Record<string, any>[] = []
     const stream = fs.createReadStream(filePath)
 
+    const csvHeaderFormatter = ({
+      header,
+    }: {
+      header: string
+    }): string | null => {
+      header = header.trim().toLowerCase()
+      header = header
+        .replaceAll(" ", "_")
+        .replaceAll(".", "_")
+        .replaceAll("(", "_")
+        .replaceAll(")", "")
+      if (header === "") return null
+      return header
+    }
+
+    const csvValueFormatter = ({
+      header,
+      value,
+    }: {
+      header: string
+      value: string
+    }): number | string | Date | null => {
+      value = value.trim() // remove leading/trailing whitespace
+      if (value === "") {
+        return null
+      }
+      if (header === "time") {
+        return value.replaceAll(".", ":")
+      }
+      if (header === "date") {
+        const [day, month, year] = value.split("/")
+        return new Date(`${year}-${month}-${day}`)
+      }
+      value = value.replace(",", ".") // replace comma with dot for decimal values
+      const numValue = parseFloat(value)
+      return isNaN(numValue) ? value : numValue
+    }
+
+    const onDataHandler = (data: Record<string, any>) => {
+      // skip records with null values
+      if (Object.values(data).every((value) => value === null)) {
+        // console.warn("Skipping record with null values:", data)
+        return
+      }
+
+      // combine date and time into a timestamp
+      const timestamp: Date = new Date(data.date)
+      if (data.time) {
+        const timeParts = data.time.split(":")
+        timestamp.setHours(
+          parseInt(timeParts[0], 10),
+          parseInt(timeParts[1], 10),
+          parseInt(timeParts[2] || "0", 10)
+        )
+      }
+      data.timestamp = timestamp.getTime()
+      // remove date and time fields after combining
+      delete data.date
+      delete data.time
+
+      // convert all -200 values to null
+      for (const [key, value] of Object.entries(data)) {
+        if (value === -200 || value === "-200") {
+          data[key] = null
+        }
+      }
+
+      chunks.push(data)
+
+      if (chunks.length === chunkSize) {
+        stream.pause()
+        // create a copy of chunks to pass to chunkHandler
+        chunkHandler([...chunks])
+          .then(() => {
+            stream.resume()
+          })
+          .catch((error) => {
+            stream.destroy()
+            reject(error)
+          })
+        chunks = [] // clear chunks immediately, if not fails
+      }
+    }
+
+    const flushData = () => {
+      if (chunks.length > 0) {
+        chunkHandler([...chunks])
+          .then(() => resolve())
+          .catch((error) => reject(error))
+        chunks.length = 0 // clear chunks after processing
+      } else {
+        resolve()
+      }
+    }
+
     stream
       .pipe(
         csvParser({
           separator: ";",
-          mapHeaders: ({ header }) => {
-            header = header.trim().toLowerCase()
-            header = header
-              .replaceAll(" ", "_")
-              .replaceAll(".", "_")
-              .replaceAll("(", "_")
-              .replaceAll(")", "")
-            if (header === "") return null
-            return header
-          },
-          mapValues: ({ header, value }) => {
-            value = value.trim() // remove leading/trailing whitespace
-            if (value === "") {
-              return null
-            }
-            if (header === "time") {
-              return value.replaceAll(".", ":")
-            }
-            if (header === "date") {
-              const [day, month, year] = value.split("/")
-              return new Date(`${year}-${month}-${day}`)
-            }
-            value = value.replace(",", ".") // replace comma with dot for decimal values
-            const numValue = parseFloat(value)
-            return isNaN(numValue) ? value : numValue
-          },
+          mapHeaders: csvHeaderFormatter,
+          mapValues: csvValueFormatter,
         })
       )
-      // .on("headers", (headers) => {
-      //   console.log(`CSV headers: ${headers.join(", ")}`)
-      // })
-      .on("data", (data) => {
-        // skip records with null values
-        if (Object.values(data).every((value) => value === null)) {
-          // console.warn("Skipping record with null values:", data)
-          return
-        }
-
-        // combine date and time into a timestamp
-        data.timestamp = new Date(data.date)
-        if (data.time) {
-          const timeParts = data.time.split(":")
-          data.timestamp.setHours(
-            parseInt(timeParts[0], 10),
-            parseInt(timeParts[1], 10),
-            parseInt(timeParts[2] || "0", 10)
-          )
-        }
-        // remove date and time fields after combining
-        delete data.date
-        delete data.time
-
-        // convert all -200 values to null
-        for (const [key, value] of Object.entries(data)) {
-          if (value === -200 || value === "-200") {
-            data[key] = null
-          }
-        }
-
-        chunks.push(data)
-
-        if (chunks.length === chunkSize) {
-          stream.pause()
-          // create a copy of chunks to pass to chunkHandler
-          chunkHandler([...chunks])
-            .then(() => {
-              stream.resume()
-            })
-            .catch((error) => {
-              stream.destroy()
-              reject(error)
-            })
-          chunks = [] // clear chunks immediately, if not fails
-        }
-      })
-      .on("end", () => {
-        if (chunks.length > 0) {
-          chunkHandler([...chunks])
-            .then(() => resolve())
-            .catch((error) => reject(error))
-          chunks.length = 0 // clear chunks after processing
-        } else {
-          resolve()
-        }
-      })
+      .on("data", onDataHandler)
+      .on("end", flushData)
       .on("error", (error) => reject(error))
   })
 }
