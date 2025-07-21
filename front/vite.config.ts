6bf85d5a659f199537054476acb582375d3fb626
Julio Cesar <juliocesar.pg92@gmail.com>
2025-07-21 09:04:18 +0200

    chore(front): :fire: remove .env.example file and update vite.config.ts to load environment variables dynamically

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,13 +1,22 @@
 import path from "node:path"
 import tailwindcss from "@tailwindcss/vite"
 import vue from "@vitejs/plugin-vue"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
-export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // Load env file from root directory
+  const env = loadEnv(mode, path.resolve(__dirname, "../"), "")
+
+  return {
+    plugins: [vue(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    envDir: "../", // Look for .env files in the root directory
+    server: {
+      port: parseInt(env.FRONT_PORT) || 5173,
+    },
+  }
 })
